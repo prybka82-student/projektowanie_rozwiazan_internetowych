668bfaf32621343875ae88a93be8e746d6b1d466
Piotr Rybka <piotr.rybka82@gmail.com>
2021-10-20 00:03:25 +0200

    Initial commit

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module Zadanie03
+module zadanie
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -5,28 +5,18 @@ import (
 	"log"
 	"net/http"
 	"runtime/debug"
+	"strconv"
 
 	"github.com/gorilla/mux"
-
-	calc "Zadanie03/calc"
 )
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", homeHandler)
-
-	r.HandleFunc("/calc/{a:[-]?[0-9]+[.,]?[0-9]*}/plus/{b:[-]?[0-9]+[.,]?[0-9]*}", addHandler).Methods("GET")
-	r.HandleFunc("/calc/{a:[-]?[0-9]+[.,]?[0-9]*}/minus/{b:[-]?[0-9]+[.,]?[0-9]*}", subHandler).Methods("GET")
-	r.HandleFunc("/calc/{a:[-]?[0-9]+[.,]?[0-9]*}/times/{b:[-]?[0-9]+[.,]?[0-9]*}", multHandler).Methods("GET")
-	r.HandleFunc("/calc/{a:[-]?[0-9]+[.,]?[0-9]*}/divide[d]?by/{b:[-]?[0-9]+[.,]?[0-9]*}", divHandler).Methods("GET")
-
-	r.HandleFunc("/calc/{a:[-]?[0-9]+[.,]?[0-9]*}/tothepowerof/{b:[-]?[0-9]+[.,]?[0-9]*}", powHandler).Methods("GET")
-
-	r.HandleFunc("/const/pi", piHandler).Methods("GET")
-	r.HandleFunc("/const/2pi", twoPiHandler).Methods("GET")
-	r.HandleFunc("/const/pisqrd", piSqrdHandler).Methods("GET")
-	r.HandleFunc("/const/e", eHandler).Methods("GET")
+	r.HandleFunc("/add/{a}/{b}", addHandler).Methods("GET")
+	r.HandleFunc("/sub/{a}/{b}", subHandler).Methods("GET")
+	r.HandleFunc("/mul/{a}/{b}", mulHandler).Methods("GET")
+	r.HandleFunc("/div/{a}/{b}", divHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", recoverMw(r, true)))
 }
@@ -75,116 +65,106 @@ func recoverMw(app http.Handler, isDev bool) http.HandlerFunc {
 	}
 }
 
-func getParams(params map[string]string) (string, string) {
-	return params["a"], params["b"]
-}
-
-func write(w http.ResponseWriter, res string) {
-	w.Write([]byte(res))
-}
-
 func addHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.Add(getParams(mux.Vars(r)))
+	params := mux.Vars(r)
 
-	write(w, res)
+	defer func() string {
+		if err := recover(); err != nil {
+			return fmt.Sprintf("Cannot add %v to %v", params["a"], params["b"])
+		}
+		return ""
+	}()
+
+	a, err := strconv.ParseFloat(params["a"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["a"]))
+		return
+	}
+
+	b, err := strconv.ParseFloat(params["b"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["b"]))
+		return
+	}
+
+	w.Write([]byte(strconv.FormatFloat(a+b, 'f', -1, 64)))
 }
 
 func subHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.Subtract(getParams(mux.Vars(r)))
+	params := mux.Vars(r)
 
-	write(w, res)
+	defer func() string {
+		if err := recover(); err != nil {
+			return fmt.Sprintf("Cannot subtract %v from %v", params["b"], params["a"])
+		}
+		return ""
+	}()
+
+	a, err := strconv.ParseFloat(params["a"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["a"]))
+		return
+	}
+
+	b, err := strconv.ParseFloat(params["b"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["b"]))
+		return
+	}
+
+	w.Write([]byte(strconv.FormatFloat(a-b, 'f', -1, 64)))
 }
 
-func multHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.Multiply(getParams(mux.Vars(r)))
+func mulHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
-	write(w, res)
+	defer func() string {
+		if err := recover(); err != nil {
+			return fmt.Sprintf("Cannot multiply %v by %v", params["a"], params["b"])
+		}
+		return ""
+	}()
+
+	a, err := strconv.ParseFloat(params["a"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["a"]))
+		return
+	}
+
+	b, err := strconv.ParseFloat(params["b"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["b"]))
+		return
+	}
+
+	w.Write([]byte(strconv.FormatFloat(a*b, 'f', -1, 64)))
 }
 
 func divHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.Divide(getParams(mux.Vars(r)))
+	params := mux.Vars(r)
 
-	write(w, res)
-}
+	defer func() string {
+		if err := recover(); err != nil {
+			return fmt.Sprintf("Cannot divide %v by %v", params["a"], params["b"])
+		}
+		return ""
+	}()
 
-func powHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.Power(getParams(mux.Vars(r)))
-
-	write(w, res)
-}
-
-func piHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.Pi()
-
-	write(w, res)
-}
-
-func twoPiHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.TwoPi()
-
-	write(w, res)
-}
-
-func piSqrdHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.PiSqrd()
-
-	write(w, res)
-}
-
-func eHandler(w http.ResponseWriter, r *http.Request) {
-	res := calc.E()
-
-	write(w, res)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`
-	<style>
-	page {
-		font-family: Arial, Helvetica, sans-serif;
+	a, err := strconv.ParseFloat(params["a"], 64)
+	if err != nil {
+		w.Write([]byte("cannot parse " + params["a"]))
+		return
 	}
-	table {
-		width: 100%;
-		border: 1px solid #000;
+	b, err := strconv.ParseFloat(params["b"], 64)
+	if err != nil {
+		w.Write([]byte("Cannot parse " + params["b"]))
+		return
 	}
-	th, td {
-		width: 50%;
-		text-align: left;
-		vertical-align: top;
-		border-spacing: 0;
-		padding: 0.3em;
+
+	if b == 0 {
+		w.Write([]byte("Cannot divide by 0"))
+		return
 	}
-	th {
-		border: 1px solid #000;
-		font-weight: bold;
-	}
-	code {
-		font-family: Monaco, "Lucida Console", monospace;
-	}
-	</style>
-	<page>
-	<h1>Online calculator</h1>
 
-	<h2>Operations</h2>
-
-	<table>
-		<tr><th>Operation</th>		<th>URL syntax</th></tr>
-		<tr><td>Adding</td>			<td><code>/calc/-123.123/plus/-123.123</code></td></tr>
-		<tr><td>Subtracking</td>	<td><code>/calc/-123.123/minus/-123.123</code></td></tr>
-		<tr><td>Multiplying</td>	<td><code>/calc/-123.123/times/-123.123</code></td></tr>
-		<tr><td>Dividing</td>		<td><code>/calc/-123.123/dividedby/-123.123</code></td></tr>
-		<tr><td>Exponentiation</td>	<td><code>/calc/-123.123/tothepowerof/-123.123</code></td></tr>
-	</table>
-
-	<h2>Constants</h2>
-
-	<table>
-		<tr><th>Constant</th><th>URL syntax</th></tr>
-		<tr><td>π</td><td><code>/const/pi</code></td></tr>
-		<tr><td>2π</td><td><code>/const/2pi</code></td></tr>
-		<tr><td>√π</td><td><code>/const/pisqrd</code></td></tr>
-		<tr><td>e</td><td><code>/const/e</code></td></tr>
-	</table>
-	</page>
-	`))
+	w.Write([]byte(strconv.FormatFloat(a/b, 'f', -1, 64)))
 }
